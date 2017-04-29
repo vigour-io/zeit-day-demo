820801024e219aa01e297f0ce6d2a6bba13ea596
Youri Daamen <me@youz.io>
2017-04-29 10:11:37 -0700

    added task for hub

--- task.dev.js
+++ task.dev.js
@@ -23,22 +23,6 @@ exports.tasks = {
       }
     }
   },
-  'build-hub': {
-    type: 'build-js',
-    options: {
-      entry: 'hub/index.js',
-      dest: 'hub/dist/index.js',
-      raw: true,
-      targets: [ 'node' ]
-    }
-  },
-  'run-hub': {
-    type: 'child-process',
-    after: [ 'build-hub' ],
-    options: {
-      cmd: 'node ./hub/dist/index.js'
-    }
-  },
   'serve-app': {
     type: 'child-process',
     options: {
